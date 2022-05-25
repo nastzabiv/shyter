ccb1e5326b9d265ff2a7ce09f7685ae73438421a
nastzabiv <105552723+nastzabiv@users.noreply.github.com>
2022-05-25 19:59:22 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from curses import KEY_ENTER
 from pygame import *
 from random import randint
 import notmain as cl
@@ -33,25 +32,24 @@ def main():
     finish = False
     run = True
     while run:
-        keys = key.get_pressed()
-        for e in event.get():
-            if e.type == QUIT or keys[K_ESCAPE]:
-                run = False
-            if e.type == K_KP_ENTER:
-                run = True
-            if e.type == KEYUP:
-                init()
 
-            elif e.type == KEYDOWN:
-                if e.key == K_SPACE:
-                    #fire_sound.play()
-                    player.fire()
-
-        if cl.lost > 3:
-            finish = True
-            
         if not finish:
+            keys = key.get_pressed()
+            for e in event.get():
+                if e.type == QUIT or keys[K_ESCAPE]:
+                    run = False
+                
+                elif e.type == KEYDOWN:
+                    if e.key == K_SPACE:
+                        #fire_sound.play()
+                        player.fire()
+
+            if cl.lost > 3:
+                finish = True
+                
+
             cl.main_win.blit(bg, (0,0))
+
 
             
             player.update()
@@ -85,22 +83,43 @@ def main():
                 cl.main_win.blit(text, (250, 560))
                 cl.main_win.blit(piclose, (300,300))
                 cl.main_win.blit(anger, (475,245))
+                cl.score = 0
                 #вывод на экран надписи
 
             #проверка выигрыша
             if cl.score >= cl.max_score:
-                finish = True
                 text = font_UI.render("You win", True, (0,102,0))
                 cl.main_win.blit(text, (350, 250))
                 text = font_UI.render("press enter for restart", True, (0,0,0))
                 cl.main_win.blit(text, (250, 560))
                 cl.main_win.blit(picwin, (300,300))
                 cl.main_win.blit(happy, (475,245))
+                cl.score = 0
+                finish = True
                 #вывод на экран надписи
 
 
             display.update()
-            clock.tick(FPS)
+        else:
+            keys = key.get_pressed()
+            for e in event.get():
+                if e.type == KEYDOWN:
+                    if e.key == K_RETURN:
+                        player = cl.Player('chmon.png', cl.win_width/2-25, cl.win_height-105, 50, 100, 5)
+                        piclose = transform.scale(image.load('crazydog.jpg'), (250,250))
+                        picwin = transform.scale(image.load('angrycat.jpg'), (250,250))
+                        anger = transform.scale(image.load('anger.png'), (50,50))
+                        happy = transform.scale(image.load('happy1.png'), (50,50))
+
+
+                        #группа врагов
+                        enemies = sprite.Group()
+                        for i in range(3):
+                            enemy = cl.Enemy("enemies.png", randint(0, cl.win_width-80), -50, 80, 50, randint(1,3))
+                            enemies.add(enemy)
+                        finish = False
+                        break
+        clock.tick(FPS)
 
 if __name__ == '__main__':
     main()
